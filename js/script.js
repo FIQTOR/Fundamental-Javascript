f8e1410ebc349e8ae6a932314f4aa47468fc0aa1
FIQTOR <117913964+FIQTOR@users.noreply.github.com>
2024-02-20 09:07:04 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// Ini adalah alert
+// alert('berhasil')
+
+// Variable
+var oke = 25
+let oke2 = 50
+const oke3 = 100
+
+// alert(`berhasil ${oke3}`)
+
+// Tipe data
+var num = 5
+var str = '564'
+var bool = false
+var array = ['Apel', 'Anggur', 'Nanas']
+
+// console.log(typeof num);
+// console.log(typeof x);
+// console.log(typeof bool);
+// console.log(typeof array);
+
+// Aritmatika
+var x = 33
+var y = 5
+
+// Shift + alt + panah ke bawah
+var pertambahan = x + y
+var pengurangan = x - y
+var perkalian = x * y
+var pembagian = x / y
+var hasilbagi = x % y
+
+console.log(pertambahan);
+console.log(pengurangan);
+console.log(perkalian);
+console.log(pembagian);
+console.log(hasilbagi);
+
+// Kondisi
+
+var nilai = 100
+
+if (nilai > 50) {
+  alert('Belajar lagi')
+}
+
+if (nilai == 100) {
+  alert('bagus')
+}
+
+// var warna = 'ungu'
+
+// switch (warna) {
+//   case 'biru':
+//     alert(`Warna adalah ${warna}`)
+//     break
+//   case 'kuning':
+//     alert(`Warna adalah ${warna}`)
+//     break
+//   case 'hijau':
+//     alert(`Warna adalah ${warna}`)
+//     break
+//   case 'ungu':
+//     alert(`Warna adalah ${warna}`)
+//     break
+//   default:
+//     alert('Warna tidak ada')
+//     break
+// }
+
+// Ternary
+// var nilai = 30
+// var pesan = (nilai > 50) ? 'belajar lagi' : 'nilai terlalu rendah'
+// alert(pesan)
